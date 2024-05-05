632dff2ac5cb77004e7c4c02921fb10fb3b97921
tobiolusa <olusamiracle@gmail.com>
2024-05-05 23:56:04 +0100

    View updated

--- dashboard/views.py
+++ dashboard/views.py
@@ -7,22 +7,22 @@ def chart(request):
     
     datetime_today = date.today()
     date_today = str(datetime_today)
-    date_10daysago = str(datetime_today - timedelta(days=10))
-    api = "https://api.coindesk.com/v1/bpi/historical/close.json?start=' + date_10daysago + '&end=' + date_today + '&index=[USD]"
+    date_10daysago = str(datetime_today - timedelta(days=20))
+    api = "https://api.coindesk.com/v1/bpi/historical/close.json"
     
     try: 
         response = requests.get(api, timeout=5)
         response.raise_for_status()
         prices = response.json()
         btc_prices_range = prices.get('bpi')
-    except request.Expection.ConnectionError as errc:
+    except requests.exceptions.ConnectionError as errc:
         raise ConnectionError(errc)
-    except request.expections.Timeout as errt:
+    except requests.exceptions.Timeout as errt:
         raise TimeoutError(errt)
-    except request.exceptions.HTTPError as err:
+    except requests.exceptions.HTTPError as err:
         raise SystemExit(err)
     
     context = {
         'price': btc_prices_range
     }
-    return render(request, 'base.html', context)
+    return render(request, 'chart.html', context)
