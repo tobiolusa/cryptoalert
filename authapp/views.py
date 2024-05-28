27ae5260a04baf344f8083a1516d71aac57c4f16
tobiolusa <olusamiracle@gmail.com>
2024-05-28 14:10:02 +0100

    registration portal activated and template ui updated

--- authapp/views.py
+++ authapp/views.py
@@ -1,14 +1,38 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import login
+from django.contrib import messages
+from django import forms
 
 def index(request):
     return render(request, "authapp/login.html")
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        username
+        email = request.POST.get('email')
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
 
+        if not (first_name and last_name and username and email and password):
+            messages.error("Please fill out all fields")
+        else:
+            if User.objects.filter(username=username).exists():
+                messages.error("Username already existed. ")
+            elif User.objects.filter(email=email).exists():
+                messages.error("Email address already existed")
+            else:
+                user = User.objects.create_user(
+                    email=username,
+                    first_name=first_name,
+                    last_name=last_name,
+                    username=username,
+                    password=password
+                )
+                login(request, user)
+                messages.success(request, 'Account Created Successfully.')
+                return redirect('login')
     return render(request, "authapp/register.html")
 
 def reset_password(request):
