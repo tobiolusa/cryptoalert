1a075e6e60b44a9c4521c5ef6a2e9e47138a2d0e
tobiolusa <olusamiracle@gmail.com>
2024-05-28 20:13:24 +0100

    flash messages updated

--- authapp/urls.py
+++ authapp/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from authapp import views 
 
 urlpatterns = [
-    path('loginuser', views.loginuser, name="loginuser"), 
+    path('', views.loginuser, name="loginuser"), 
     path('register/', views.register, name="register"),
     path('reset-password/', views.reset_password, name="reset-password"),
     path('logout/', views.logout_user, name="logout")
--- cryptoproject/settings.py
+++ cryptoproject/settings.py
@@ -132,5 +132,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-LOGIN_URL ='loginuser'
+LOGIN_URL ='/'
 LOGOUT_REDIRECT_URL = 'chart'
