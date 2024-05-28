373196ada8916024948b2538aa94eb839990759f
tobiolusa <olusamiracle@gmail.com>
2024-05-28 11:07:58 +0100

    auth init

--- authapp/views.py
+++ authapp/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 def index(request):
     return render(request, "authapp/login.html")
 
 def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        username
+
     return render(request, "authapp/register.html")
 
 def reset_password(request):
     return render(request, "authapp/reset-password.html")
+
+
